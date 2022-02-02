9fa20d6ca93477b295a1492858ba6727cc2f6c44
nhuvu98 <nguyennhuvu98@gmail.com>
2022-02-02 18:09:07 +0700

    config load resource image

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'game-bundle.js',
-  }
+    clean : true
+  },
+  module : {
+      rules : [
+          {
+              test : /\.css$/i,
+              use : ['style-loader', 'css-loader']
+          },
+          {
+              test : /\.(png|svg|jpg|jpeg|gif)$/i,
+              type : 'asset/resource'
+          }
+      ]
+  },
+  plugins : [
+      new HtmlWebpackPlugin({
+          title : 'Output Manager',
+      }),
+    ]
 };
